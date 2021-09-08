46914f94d04fb38995aad6bb121d3c00b7616e73
Zateev Aleksey <alekseyzateev@gmail.com>
2021-09-08 11:16:25 +0300

    manually made user authentication

--- src/main/java/ru/zateev/springsecuritytest/conf/ConfigurationSecurity.java
+++ src/main/java/ru/zateev/springsecuritytest/conf/ConfigurationSecurity.java
@@ -1,8 +1,17 @@
 package ru.zateev.springsecuritytest.conf;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 
@@ -10,6 +19,43 @@ public class ConfigurationSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().denyAll().and().formLogin().and().httpBasic();
+                .antMatchers("/account").authenticated()
+                .antMatchers("/balance").authenticated()
+                .antMatchers("/loans").authenticated()
+                .antMatchers("/notices").permitAll()
+                .antMatchers("/contact").permitAll()
+                .antMatchers("/card").authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
+    /*
+    *кастомные юзеры - создание, а не через spring boot
+    * храниться они будут в памяти security
+    * passwordencoder - у которого нет пароля
+    * */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication().withUser("user").password("12345")
+//                .authorities("user")
+//                .and()
+//                .withUser("admin").password("1234").authorities("admin")
+//                .and()
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+
+        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+        UserDetails user = User.withUsername("user").password("12345").authorities("user").build();
+        UserDetails admin = User.withUsername("admin").password("12345").authorities("admin").build();
+        userDetailsManager.createUser(user);
+        userDetailsManager.createUser(admin);
+        auth.userDetailsService(userDetailsManager);
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
